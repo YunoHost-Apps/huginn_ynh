62dc38addab9b0ccfea31332c4d8061a580d9f4f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-09-28 22:31:48 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies=""
-#REMOVEME? build_pkg_dependencies="default-libmysqlclient-dev libyaml-dev libgdbm-dev libncurses5-dev libffi-dev libcurl4-openssl-dev libicu-dev python3-docutils pkg-config cmake nodejs graphviz jq"
-
 ruby_version=2.6
 
 #=================================================
